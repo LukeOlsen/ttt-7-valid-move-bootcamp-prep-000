870d8b45bd9459ea3a485b639894d752d801114e
Luke Olsen <lukeaolsen@gmail.com>
2018-10-09 02:11:49 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,14 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  correct_index = index.to_i - 1
-  
-
-  
-  if position_taken(board, correct_index)
-    return "That position is already taken!"
+  if (index > 0 && index < 10)
+    correct_index = index.to_i - 1
+    if position_taken(board, correct_index)
+      return "That position is already taken!"
+    else board[correct_index] = "X"
+    end
   end
 end
-
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
