81496ffc138d3857f2ef912533a93c453b3ecab6
Luke Olsen <lukeaolsen@gmail.com>
2018-10-09 02:09:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken(board, index)
+  correct_index = index.to_i - 1
+  
+
+  
+  if position_taken(board, correct_index)
     return "That position is already taken!"
   end
 end
